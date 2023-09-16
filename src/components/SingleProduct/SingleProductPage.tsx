571ff61892d6a26d039ed6c4647fdeb5d5a05848
Daniel <danielwandurua@gmail.com>
2023-09-16 13:59:58 +0300

    make it responsive

--- src/components/SingleProduct/SingleProductPage.tsx
+++ src/components/SingleProduct/SingleProductPage.tsx
@@ -18,49 +18,55 @@ const product = {
 const SingleProductPage = () => {
   const [selectedImage, setSelectedImage] = useState<number>(0);
   return (
-    <section className="flex items-center justify-center py-8 px-12">
-      <div className="flex items-center">
-        <div className="flex flex-col flex-wrap gap-2 p-2 m-4">
-          {product.images.map((image, index) => (
-            <img
-              src={image}
-              alt={product.title}
-              key={index}
-              onClick={() => setSelectedImage(index)}
-              className={
-                selectedImage === index
-                  ? "w-20 h-20 object-cover rounded-md cursor-pointer transition-all duration-[0.2s] ease-in-out scale-[1.12] "
-                  : "w-20 h-20 object-cover rounded-md cursor-pointer transition-all duration-[0.2s] ease-in-out"
-              }
-            />
-          ))}
-        </div>
+    <section className="flex flex-col md:flex-row items-center justify-center py-8 px-4 lg:px-12">
+      <div className="flex md:flex-col flex-wrap gap-2 p-2 m-4">
+        {product.images.map((image, index) => (
+          <img
+            src={image}
+            alt={product.title}
+            key={index}
+            onClick={() => setSelectedImage(index)}
+            className={`
+            w-20 h-20 object-cover rounded-md cursor-pointer transition-transform duration-200 ease-in-out
+            ${selectedImage === index ? "scale-[1.12]" : ""}
+          `}
+          />
+        ))}
+      </div>
+      <div className="w-full md:w-2/5 lg:w-1/2 p-4">
         <img
           src={product.images[selectedImage]}
           alt={product.title}
-          className="w-[500px] h-[500px] object-cover rounded-xl"
+          className="w-full h-auto md:h-[400px] lg:h-[500px] object-cover rounded-xl"
         />
       </div>
-      <div className="w-[35%] py-4 px-6">
-        <h1 className="text-xl font-bold mb-3">{product.title}</h1>
-        <p className="mb-4 leading-[1.4]">{product.description}</p>
+
+      <div className="w-full md:w-3/5 lg:w-1/3 p-4">
+        <h1 className="text-xl md:text-2xl lg:text-3xl font-bold mb-3">
+          {product.title}
+        </h1>
+        <p className="mb-4 leading-relaxed text-gray-600">
+          {product.description}
+        </p>
         <p className="mb-4 text-2xl font-semibold">
           ${product.price.toFixed(2)}
         </p>
-        <h2 className=" text-xl font-bold">Quantity:</h2>
+        <h2 className="text-xl font-bold">Quantity:</h2>
         <div className="flex items-center text-xl font-bold mt-1 mb-4">
           <button
-            className=" w-9 h-9 text-2xl bg-[#ff8848] text-white border-none rounded-full cursor-pointer disabled:opacity-30 disabled:cursor-default"
+            className="w-9 h-9 text-2xl bg-[#ff8848] text-white border-none rounded-full cursor-pointer disabled:opacity-30 disabled:cursor-default"
             disabled
           >
             -
           </button>
-          <p className="mx-10 text-center">1</p>
+          <p className="mx-4 md:mx-6 lg:mx-10 text-center">1</p>
           <button className="w-9 h-9 text-2xl bg-[#ff8848] text-white border-none rounded-full cursor-pointer">
             +
           </button>
         </div>
-        <button className="search_button w-fit py-2 px-4">Add to Cart</button>
+        <button className="w-full md:w-auto bg-[#ff8848] text-white py-2 px-4 rounded-md shadow-md hover:shadow-lg focus:outline-none focus:ring-2 focus:ring-[#ff8848]">
+          Add to Cart
+        </button>
       </div>
     </section>
   );
